82dac9ae36212b2dc32ca419b8c860a79eaa3c50
Victor <victor.gonzalez@cern.ch>
2022-05-19 18:46:35 +0200

    Pythia analysis for the whole pT range

--- Pythia/RunPythiaSimulationTwoParticlesDiff.cpp
+++ Pythia/RunPythiaSimulationTwoParticlesDiff.cpp
@@ -33,10 +33,10 @@ int main(int argc, char* argv[])
   // =========================
   float min_eta = -1;
   float max_eta = 1;
-  float min_pt = 0.2;
-  float max_pt = 2.5;
+  float min_pt = 0.0;
+  float max_pt = 1e6;
   int nBins_eta = int((max_eta - min_eta) / 0.1);
-  int nBins_pt = int((max_pt - min_pt) / 0.1);
+  // int nBins_pt = int((max_pt - min_pt) / 0.1);
 
   long nEventsRequested = 1000000;
   int nEventsReport = 10000;
@@ -52,7 +52,7 @@ int main(int argc, char* argv[])
   // ==========================
   int nOptions = 0;
   TString** pythiaOptions = new TString*[50];
-  /* no decay of strange barions */
+  /* no decay of strange barions
   pythiaOptions[nOptions++] = new TString("130:mayDecay = off");
   pythiaOptions[nOptions++] = new TString("310:mayDecay = off");
   pythiaOptions[nOptions++] = new TString("311:mayDecay = off");
@@ -62,7 +62,7 @@ int main(int argc, char* argv[])
   pythiaOptions[nOptions++] = new TString("3322:mayDecay = off");
   pythiaOptions[nOptions++] = new TString("3312:mayDecay = off");
   pythiaOptions[nOptions++] = new TString("3334:mayDecay = off");
-  pythiaOptions[nOptions++] = new TString("3122:mayDecay = off");
+  pythiaOptions[nOptions++] = new TString("3122:mayDecay = off"); */
 
   pythiaOptions[nOptions++] = new TString("Init:showChangedSettings = on");      // list changed settings
   pythiaOptions[nOptions++] = new TString("Init:showChangedParticleData = on");  // list changed particle data
@@ -107,7 +107,12 @@ int main(int argc, char* argv[])
   ac->rootOuputFileName = TString::Format("PYTHIA8_Pairs_%03d_", jobix).Data();
   ac->histoBaseName = "TEST";
 
-  ac->nBins_pt = nBins_pt;
+  ac->bin_edges_pt = {0.0,
+                      0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55,
+                      0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.0, 1.1, 1.2, 1.3, 1.4,
+                      1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4,
+                      3.6, 3.8, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 8.0, 10.0, 13.0, 20.0};
+  ac->nBins_pt = ac->bin_edges_pt.size() - 1;
   ac->min_pt = min_pt;
   ac->max_pt = max_pt;
   ac->nBins_eta = nBins_eta;
@@ -138,13 +143,10 @@ int main(int argc, char* argv[])
   particleFilters.push_back(new ParticleFilter(ParticleFilter::Kaon, ParticleFilter::Negative, ac->min_pt, ac->max_pt, ac->min_eta, ac->max_eta, ac->min_y, ac->max_y));
   particleFilters.push_back(new ParticleFilter(ParticleFilter::Proton, ParticleFilter::Positive, ac->min_pt, ac->max_pt, ac->min_eta, ac->max_eta, ac->min_y, ac->max_y));
   particleFilters.push_back(new ParticleFilter(ParticleFilter::Proton, ParticleFilter::Negative, ac->min_pt, ac->max_pt, ac->min_eta, ac->max_eta, ac->min_y, ac->max_y));
-  particleFilters.push_back(new ParticleFilter(ParticleFilter::Lambda, ParticleFilter::Neutral, ac->min_pt, ac->max_pt, ac->min_eta, ac->max_eta, ac->min_y, ac->max_y));
-  particleFilters.push_back(new ParticleFilter(ParticleFilter::ALambda, ParticleFilter::Neutral, ac->min_pt, ac->max_pt, ac->min_eta, ac->max_eta, ac->min_y, ac->max_y));
-  particleFilters.push_back(new ParticleFilter(ParticleFilter::Photon, ParticleFilter::Neutral, ac->min_pt, ac->max_pt, ac->min_eta, ac->max_eta, ac->min_y, ac->max_y));
 
   int iTask = 0;
   /* the two-particle analyzer */
-  analysisTasks[iTask++] = new TwoPartDiffCorrelationAnalyzer("NarrowPiKaPrLa", ac, event, eventFilter, particleFilters);
+  analysisTasks[iTask++] = new TwoPartDiffCorrelationAnalyzer("NarrowPiKaPr", ac, event, eventFilter, particleFilters);
   analysisTasks[iTask - 1]->reportLevel = logLevel;
 
   /* single particle analysis filters */
@@ -152,8 +154,9 @@ int main(int argc, char* argv[])
   ParticleFilter** singleParticleFilters = new ParticleFilter*[nParticleFilters];
   singleParticleFilters[0] = new ParticleFilter(ParticleFilter::AllSpecies, ParticleFilter::AllCharges, ac->min_pt, ac->max_pt, ac->min_eta, ac->max_eta, ac->min_y, ac->max_y);
   analysisTasks[iTask++] = new ParticleAnalyzer("SinglesPythia", ac, event, eventFilter, nParticleFilters, singleParticleFilters);
-  analysisTasks[iTask - 1]->reportLevel = logLevel;
-  nAnalysisTasks = iTask; */
+  analysisTasks[iTask - 1]->reportLevel = logLevel; */
+
+  nAnalysisTasks = iTask;
 
   // ==========================
   // Event Loop
